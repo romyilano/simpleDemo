b44bbc7e0d5142c37d5f50f87b7baec6a44b0143
romy <romy@snowyla.com>
2023-08-03 11:41:51 -0700

    jupyter notebook

---
+++ jupyter/sample.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# loading the data from CSV
+df = pd.read_csv('sample.csv')
+
+# setting the figure size for the plot
+plt.figure(figsize=(10, 
